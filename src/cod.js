87961b06e036ca4186149b2674a0c778b39303ac
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2024-03-12 19:57:36 -0700

    Uncommented section of Cod.js

--- src/cod.js
+++ src/cod.js
@@ -22,7 +22,7 @@ async function compileFromFile(filename, outputType) {
   try {
     const buffer = await fs.readFile(filename)
     const compiled = compile(buffer.toString(), outputType)
-    console.log(/*compiled instanceof Program ? stringify(compiled) :*/ compiled)
+    console.log(compiled instanceof Program ? stringify(compiled) : compiled)
   } catch (e) {
     console.error(`\u001b[31m${e}\u001b[39m`)
     process.exitCode = 1
