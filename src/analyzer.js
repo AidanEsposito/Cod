a77c8508e2cfaa1a082dab0f0478a558e046adce
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2024-03-14 13:03:41 -0700

    started analyzer.js

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,59 @@
-export default function analyze() {
-  throw new Error("Not yet implemented");
+// The semantic analyzer exports a single function, analyze(match), that
+// accepts a grammar match object (the CST) from Ohm and produces the
+// internal representation of the program (pretty close to what is usually
+// called the AST). This representation also includes entities from the
+// standard library, as needed.
+
+import * as core from "./core.js"
+
+const INT = core.intType
+const FLOAT = core.floatType
+const STRING = core.stringType
+const BOOLEAN = core.boolType
+const VOID = core.voidType
+
+class Context {
+  // Like most statically-scoped languages, Carlos contexts will contain a
+  // map for their locally declared identifiers and a reference to the parent
+  // context. The parent of the global context is null. In addition, the
+  // context records whether analysis is current within a loop (so we can
+  // properly check break statements), and reference to the current function
+  // (so we can properly check return statements).
+  constructor({ parent = null, locals = new Map(), inLoop = false, function: f = null }) {
+    Object.assign(this, { parent, locals, inLoop, function: f })
+  }
+  add(name, entity) {
+    this.locals.set(name, entity)
+  }
+  lookup(name) {
+    return this.locals.get(name) || this.parent?.lookup(name)
+  }
+  static root() {
+    return new Context({ locals: new Map(Object.entries(core.standardLibrary)) })
+  }
+  newChildContext(props) {
+    return new Context({ ...this, ...props, parent: this, locals: new Map() })
+  }
 }
+
+export default function analyze(match) {
+  // Track the context manually via a simple variable. The initial context
+  // contains the mappings from the standard library. Add to this context
+  // as necessary. When needing to descent into a new scope, create a new
+  // context with the current context as its parent. When leaving a scope,
+  // reset this variable to the parent context.
+  let context = Context.root()
+
+  // The single gate for error checking. Pass in a condition that must be true.
+  // Use errorLocation to give contextual information about the error that will
+  // appear: this should be an object whose "at" property is a parse tree node.
+  // Ohm's getLineAndColumnMessage will be used to prefix the error message. This
+  // allows any semantic analysis errors to be presented to an end user in the
+  // same format as Ohm's reporting of syntax errors.
+  function must(condition, message, errorLocation) {
+    if (!condition) {
+      const prefix = errorLocation.at.source.getLineAndColumnMessage()
+      throw new Error(`${prefix}${message}`)
+    }
+  } //NOT DONE -- ADD MUST_____ AND ADD FUNCTIONS FOR EACH COMPONENT OF COD
+}//needs replacement
