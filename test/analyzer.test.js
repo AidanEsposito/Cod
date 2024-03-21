219d9f307502551a1bdb09e8d8ae1206671a2be3
Aidan <aesposi6@lion.lmu.edu>
2024-03-21 16:12:36 -0700

    Added more tests

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -27,9 +27,8 @@ const semanticChecks = [
   ["break in nested if", "tide unhooked{ if hooked{ snap}} "],
   ["long if", "if hooked { cast: 1} else {cast: 3}"],
   ["elsif", "if hooked {cast: 1} else if hooked { cast: 0} else {cast: 3}"],
-  //   ["for over collection", "for i in [2,3,5] {print(1);}"],
   ["for in range", "stream i in x < 10{ cast: 0 }"],
-  //   ["repeat", "repeat 3 {let a = 1; print(a);}"],
+  ["while", "tide hooked{ cast: 1} "],
   //   ["conditionals with ints", "print(true ? 8 : 5);"],
   //   ["conditionals with floats", "print(1<2 ? 8.0 : -5.22);"],
   //   ["conditionals with strings", 'print(1<2 ? "x" : "y");'],
@@ -91,21 +90,18 @@ const semanticChecks = [
 const semanticErrors = [
   //   ["non-distinct fields", "struct S {x: boolean x: int}", /Fields must be distinct/],
 ["non-int increment", "boolean x = unhooked x++", /an integer/],
-  //   ["non-int decrement", 'let x=some[""];x++;', /an integer/],
+["non-int decrement", 'land x = true x++', /an integer/],
 ["undeclared id", "cast: x", /Identifier x not declared/],
 ["redeclared id", "land x = 1 land x = 1", /Identifier x already declared/],
-  //   ["recursive struct", "struct S { x: int y: S }", /must not be self-containing/],
-  //   ["assign to const", "const x = 1;x = 2;", /Cannot assign to constant/],
   //   ["assign bad type", "let x=1;x=true;", /Cannot assign a boolean to a int/],
   //   ["assign bad array type", "let x=1;x=[true];", /Cannot assign a \[boolean\] to a int/],
-  //   ["assign bad optional type", "let x=1;x=some 2;", /Cannot assign a int\? to a int/],
-  //   ["break outside loop", "break;", /Break can only appear in a loop/],
+["break outside loop", "snap", /Break can only appear in a loop/],
   //   [
   //     "break inside function",
   //     "while true {function f() {break;}}",
   //     /Break can only appear in a loop/,
   //   ],
-  //   ["return outside function", "return;", /Return can only appear in a function/],
+["return outside function", "reel", /Return can only appear in a function/],
   //   [
   //     "return value from void function",
   //     "function f() {return 1;}",
@@ -120,14 +116,7 @@ const semanticErrors = [
   ["return type mismatch", " int test() { reel unhooked }", /boolean to a int/],
   ["non-boolean short if test", "if 1 {}", /Expected hooked or unhooked (a boolean)/],
   ["non-boolean if test", " if 1 {} else {}", /Expected a boolean/],
-  //   ["non-boolean while test", "while 1 {}", /Expected a boolean/],
-  //   ["non-integer repeat", 'repeat "1" {}', /Expected an integer/],
-  //   ["non-integer low range", "for i in true...2 {}", /Expected an integer/],
-  //   ["non-integer high range", "for i in 1..<no int {}", /Expected an integer/],
-  //   ["non-array in for", "for i in 100 {}", /Expected an array/],
-  //   ["non-boolean conditional test", "print(1?2:3);", /Expected a boolean/],
-  //   ["diff types in conditional arms", "print(true?1:true);", /not have the same type/],
-  //   ["unwrap non-optional", "print(1??2);", /Expected an optional/],
+  ["non-boolean while test", "tide 1 {}", /Expected a boolean/],
   ["bad types for ||", "cast: unhooked || 1", /Expected a boolean/],
   ["bad types for &&", "cast: unhooked && 1", /Expected a boolean/],
   ["bad types for ==", "cast: unhooked ==1", /Operands do not have the same type/],
@@ -146,10 +135,10 @@ const semanticErrors = [
   ["bad types for !=", "cast: unhooked != 1", /not have the same type/],
   ["bad types for negation", "cast: -hooked", /Expected a number/],
   //   ["bad types for length", "print(#false);", /Expected an array/],
-  //   ["bad types for not", 'print(!"hello");', /Expected a boolean/],
+  ["bad types for not", 'cast: !"hello"', /Expected a boolean/],
   //   ["bad types for random", "print(random 3);", /Expected an array/],
   //   ["non-integer index", "let a=[1];print(a[false]);", /Expected an integer/],
-  //   ["no such field", "struct S{} let x=S(); print(x.y);", /No such field/],
+  // ["no such field", "struct S{} let x=S(); print(x.y);", /No such field/],
   //   ["diff type array elements", "print([3,3.0]);", /Not all elements have the same type/],
   //   ["shadowing", "let x = 1;\nwhile true {let x = 1;}", /Identifier x already declared/],
   //   ["call of uncallable", "let x = 1;\nprint(x());", /Call of non-function/],
@@ -158,11 +147,19 @@ const semanticErrors = [
   //     "function f(x: int) {}\nf(1,2);",
   //     /1 argument\(s\) required but 2 passed/,
   //   ],
-  //   [
-  //     "Too few args",
-  //     "function f(x: int) {}\nf();",
-  //     /1 argument\(s\) required but 0 passed/,
-  //   ],
+
+
+  //fix function call!!!!
+
+    // [
+    //   "Too few args",
+    //   "function f(x: int) {}\nf();",
+    //   /1 argument\(s\) required but 0 passed/,
+    // ],
+
+
+
+
   //   [
   //     "Parameter type mismatch",
   //     "function f(x: int) {}\nf(false);",
@@ -181,10 +178,6 @@ const semanticErrors = [
   //     'function f(x: int): int {return 1;} function g(y: int): string {return "uh-oh";} f = g;',
   //     /Cannot assign a \(int\)->string to a \(int\)->int/,
   //   ],
-  //   ["bad call to sin()", "print(sin(true));", /Cannot assign a boolean to a float/],
-  //   ["Non-type in param", "let x=1;function f(y:x){}", /Type expected/],
-  //   ["Non-type in return type", "let x=1;function f():x{return 1;}", /Type expected/],
-  //   ["Non-type in field type", "let x=1;struct S {y:x}", /Type expected/],
 ]
 
 describe("The analyzer", () => {
