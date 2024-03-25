adc4d42c72fabe50bad974b7ba670f4b08b10ee1
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2024-03-25 16:13:47 -0700

    Removed comments

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -14,15 +14,9 @@ const semanticChecks = [
   ["increment and decrement", "land x = 10 x-- x++"],
   ["increment and decrement", "land x = 10; x--; x++;"],
   ["type declaration", "int x = 1, boolean y = hooked, string z = 'hello'"],
-  //   ["assign arrays", "let a = [int]();let b=[1];a=b;b=a;"],
-  //   ["assign to array element", "const a = [1,2,3]; a[1]=100;"],
-  //   ["initialize with empty optional", "let a = no int;"],
   ["short return", "string test(){ reel }"],
   ["long return", "boolean test(){ reel hooked}"],
-  //   ["assign optionals", "let a = no int;let b=some 1;a=b;b=a;"],
-
   //FIX RETURN IN OHM!
-
   ["return in nested if", "boolean f(){ if hooked{ reel}}"],
   ["break in nested if", "tide unhooked{ if hooked{ snap}} "],
   ["short if", "if hooked { cast: 1}"],
@@ -31,60 +25,21 @@ const semanticChecks = [
 
   ["for in range", "stream i in x < 10{ cast: 0 }"],
   ["while", "tide hooked{ cast: 1} "],
-  //   ["conditionals with ints", "print(true ? 8 : 5);"],
-  //   ["conditionals with floats", "print(1<2 ? 8.0 : -5.22);"],
-  //   ["conditionals with strings", 'print(1<2 ? "x" : "y");'],
-  //   ["??", "print(some 5 ?? 0);"],
-  //   ["nested ??", "print(some 5 ?? 8 ?? 0);"],
   ["||", "cast: hooked || unhooked || 1 || hello"],
   ["&&", "cast: (hooked && 1< 2 && unhooked &&!hooked)"],
+<<<<<<< HEAD
+  ["relations", 'cast:  1<= 2 && "x"> "y" &&  3.5 <1.2'],
+=======
   //   ["bit ops", "print((1&2)|(9^3));"],
   ["relations", 'cast:  1<= 2 && "x"> "y" &&  3.5 <1.2'],
   //   ["ok to == arrays", "print([1]==[5,8]);"],
   //   ["ok to != arrays", "print([1]!=[5,8]);"],
   //   ["shifts", "print(1<<3<<5<<8>>2>>0);"],
+>>>>>>> 06adef368c1c0d55c0ef9d6574df8b85f909a37e
   ["arithmetic", "land x = 1 cast: (2*3+5**-3/2-5%8)"],
-  //   ["array length", "print(#[1,2,3]);"],
-  //   ["optional types", "let x = no int; x = some 100;"],
-  //   ["random with array literals, ints", "print(random [1,2,3]);"],
-  //   ["random with array literals, strings", 'print(random ["a", "b"]);'],
-  //   ["random on array variables", "let a=[true, false];print(random a);"],
   ["variables", "land x = [[[[1]]]] cast: x + 2"],
-  //   ["pseudo recursive struct", "struct S {z: S?} let x = S(no S);"],
-  //   ["nested structs", "struct T{y:int} struct S{z: T} let x=S(T(1)); print(x.z.y);"],
-  //   ["member exp", "struct S {x: int} let y = S(1);print(y.x);"],
-  //   ["optional member exp", "struct S {x: int} let y = some S(1);print(y?.x);"],
-  //   ["subscript exp", "let a=[1,2];print(a[0]);"],
-  //   ["array of struct", "struct S{} let x=[S(), S()];"],
-  //   ["struct of arrays and opts", "struct S{x: [int] y: string??}"],
   ["assigned functions", "ocean int f(){ let g = f let f = g}"],
   ["multi param functions", "ocean int hello (int x, int y){ cast: 1}"],
-  //   ["type equivalence of nested arrays", "function f(x: [[int]]) {} print(f([[1],[2]]));"],
-  //   [
-  //     "call of assigned function in expression",
-  //     `function f(x: int, y: boolean): int {}
-  //     let g = f;
-  //     print(g(1, true));
-  //     f = g; // Type check here`,
-  //   ],
-  //   [
-  //     "pass a function to a function",
-  //     `function f(x: int, y: (boolean)->void): int { return 1; }
-  //      function g(z: boolean) {}
-  //      f(2, g);`,
-  //   ],
-  //   [
-  //     "function return types",
-  //     `function square(x: int): int { return x * x; }
-  //      function compose(): (int)->int { return square; }`,
-  //   ],
-  //   ["function assign", "function f() {} let g = f; let h = [g, f]; print(h[0]());"],
-  //   ["struct parameters", "struct S {} function f(x: S) {}"],
-  //   ["array parameters", "function f(x: [int?]) {}"],
-  //   ["optional parameters", "function f(x: [int], y: string?) {}"],
-  //   ["empty optional types", "print(no [int]); print(no string);"],
-  //   ["types in function type", "function f(g: (int?, float)->string) {}"],
-  //   ["voids in fn type", "function f(g: (void)->void) {}"],
   ["outer variable", "land x = 1 tide unhooked { cast: x }"],
 ]
 
@@ -95,7 +50,10 @@ const semanticErrors = [
   ["non-int decrement", 'land x = hooked x--', /Expected an integer/],
   ["undeclared id", "cast: x", /Identifier x not declared/],
   ["redeclared id", "land x = 1 land x = 1", /Identifier x already declared/],
+<<<<<<< HEAD
+=======
     //   ["assign bad type", "let x=1;x=true;", /Cannot assign a boolean to a int/],
+>>>>>>> 06adef368c1c0d55c0ef9d6574df8b85f909a37e
   ["break outside loop", "snap", /Break can only appear in a loop/],
   ["continue outside loop", "flow", /Continue can only appear in a loop/],
   [
@@ -145,6 +103,10 @@ const semanticErrors = [
   ["bad types for !=", "cast: unhooked != 1", /not have the same type/],
   ["bad types for negation", "cast: -hooked", /Expected a number/],
   ["bad types for not", 'cast: !"hello"', /Expected a boolean/],
+<<<<<<< HEAD
+  //FUNCTION CALL NEEDS WORK!!!
+  ["call of uncallable", "land x = 1 cast: x()", /Call of non-function/],
+=======
 
   //   ["non-integer index", "let a=[1];print(a[false]);", /Expected an integer/],
   // ["no such field", "struct S{} let x=S(); print(x.y);", /No such field/],
@@ -186,6 +148,7 @@ const semanticErrors = [
   //      f(2, g);`,
   //     /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
   //   ],
+>>>>>>> 06adef368c1c0d55c0ef9d6574df8b85f909a37e
 ]
 
 describe("The analyzer", () => {
